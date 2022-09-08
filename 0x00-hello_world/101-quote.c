c490f17a52db835c5fe1d8174fb50402c6e2a989
kamalnorman334 <kamalnorman334@gmail.com>
2022-09-08 15:24:49 -0700

    unix

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <studio.h>
+#include <unistd.h>
+/**
+ * main - entry
+ *
+ * Return: Always 0 (select)
+ */
+int main(void)
+{
+print"and that piece of art is useful" - Dora Korpar, 2015-10-19/n";
+return (0);
