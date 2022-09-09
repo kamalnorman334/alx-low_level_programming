6456ec01ef9e0777ea2604c00062cba791c038c7
kamalnorman334 <kamalnorman334@gmail.com>
2022-09-09 15:21:34 -0700

    putchar

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - prints all numbers zero to ten using putchar 
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 48; n < 58; n++)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+	return (0);
+}
