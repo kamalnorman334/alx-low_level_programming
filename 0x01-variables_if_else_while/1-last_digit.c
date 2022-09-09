91c62889da7422d51a459a747ec6ae8aa86a8093
kamalnorman334 <kamalnorman334@gmail.com>
2022-09-09 14:06:36 -0700

    files

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/* 
+ * main - Point entry
+ * Description - assign a random number to the variable
+ *
+ * Return: 0
+ *
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if int(n > 5)
+
+
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <ctype.h>
+/*
+ * main - program that prints alphabets in lower case
+ *
+ * return: o (Success)
+ */
+int main (void)
+
