a56099898c6a378a576a34d1233982c1cae95fc1
kamalnorman334 <kamalnorman334@gmail.com>
2022-09-14 12:08:55 -0700

    lowercase

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * print_alphabet - Entry point
+ * Return:0
+ */
+
+main(void)
+{
+	char chr = 'a';
+
+	while (chr <= 'z')
+	{
+		printf(chr);
+		chr++;
+
+	}
+	printf('\n');
+return (0);
+}
