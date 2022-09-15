9c926147682a68b6865f3b4be625c234716a6164
kamalnorman334 <kamalnorman334@gmail.com>
2022-09-15 13:46:28 -0700

    straight line

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -6,14 +6,25 @@
  *
  * Return: 0
  */
-
 void print_line(int n)
 {
-	int a = 0;
 
-	while (a < n && n > 0)
-	{
-		_putchar('_');
-		a++;
-	}
-	_putchar(
+
+int i;
+
+if (n > 0)
+{
+for (i = 0; i < n; i++)
+{
+_putchar (95);
+}
+
+_putchar ('\n');
+}
+
+else
+{
+_putchar ('\n');
+}
+
+}
