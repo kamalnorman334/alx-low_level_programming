4c70462eedc9b8d429e7eea4bba8014464576cd3
kamalnorman334 <kamalnorman334@gmail.com>
2022-09-20 09:14:42 -0700

    outs

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -10,13 +10,9 @@
 void _puts(char *str)
 {
 	int _putchar(char c)
-
-	int i = 0;
-
-	while (str[i])
+		for (;*str != '\0'; str++)
 	{
-		_putchar(str[i]);
-		i++;
+		_putchar(*str);
 	}
 	_putchar('\n');
 }
