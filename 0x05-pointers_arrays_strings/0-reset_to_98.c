e858fa0f8c30dbff08d107b2c7af7edff13ad13d
kamalnorman334 <kamalnorman334@gmail.com>
2022-09-19 18:31:04 -0700

    battery test

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ *reset_to_98 - integer value is updated to 98
+ *
+ *@n: A pointer to n will be updated
+ *
+ *Return Always 0
+ */
+
+void reset_to_98(int *n)
+
+{
+*n = 98;
+}
