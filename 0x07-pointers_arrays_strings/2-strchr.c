663a95a9f36d5506f35c170ec318c81ad44d1690
kamalnorman334 <kamalnorman334@gmail.com>
2022-09-26 15:52:20 -0700

    strind chr

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -5,17 +5,18 @@
  * @s: String
  * @c: Character to search
  * Return: (s)
- **/
+ *
+ */
 
+char *_strchr(char *s, char c)
 {
-	while (*s != '\0')
+	int i;
+
+	for (i = 0; s[i] >= '\0'; i++)
 	{
-		if (*s == c)
-			return (s);
-		s++;
+		if (s[i] == c)
+			return (s + i);
 	}
 
-	if (*s == c)
-		return (s);
 	return ('\0');
 }
