454b23be56df04a4225a315141c1d4675f1595c7
kamalnorman334 <kamalnorman334@gmail.com>
2022-09-28 18:38:02 -0700

    Printing a string in reverse

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * factorial - a fubtion that returns the factorial
+ * of a given number
+ * @n: int
+ * Return: factorial
+ */
+
+int factorial(int n)
+{
+
+if (n < 0)
+{
+	return (-1);
+}
+else if (n == 0)
+{
+	return (1);
+}
+
+return (n * factorial(n - 1));
+
+}
