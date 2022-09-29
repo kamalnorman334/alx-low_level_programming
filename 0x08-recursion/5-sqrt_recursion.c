d237a314bb2ecc45dcb97f3574a1b3b0a00878d6
kamalnorman334 <kamalnorman334@gmail.com>
2022-09-28 19:26:12 -0700

    A function that returns neutral square root of a number

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,13 +1,13 @@
 #include "main.h"
 
 /**
- * power_operation - a function that returns
- * the natural square root of a number
- * @n: input
- * @c: iterator.
- * Return: square root
+ * find_sqrt - Finds the natural square root of an inputted number.
+ * @num: int to find the square root of.
+ * @root: The root to be tested.
+ *
+ * Return: If the number has a natural square root - the square root.
+ *         If the number does not have a natural square root - -1.
  */
-
 int find_sqrt(int num, int root)
 {
 	if ((root * root) == num)
@@ -20,12 +20,11 @@ int find_sqrt(int num, int root)
 }
 
 /**
- * _sqrt_recursion - Returns the natural square root of a number
+ * _sqrt_recursion - Returns the natural square root of a number.
+ * @n: int to return the square root of.
  *
- * @n: input 
- *
- * Return: natural square root
- *
+ * Return: If n has a natural square root - the natural square root of n.
+ *         If n does not have a natural square root - -1.
  */
 int _sqrt_recursion(int n)
 {
