9ad138434cafbb9a795bbc83b20a7859fd9bab51
kamalnorman334 <kamalnorman334@gmail.com>
2022-09-28 19:05:36 -0700

    A function that returns neutral square root of a number

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -8,13 +8,41 @@
  * Return: square root
  */
 
+int _sqrt_recursion_wrapper(int n)
+{
+	int min
+	int max
+	int i, guess_squared;
+
+	i = (min + max) / 2;
+	i_sqr = i * i;
+
+	if (i_sqr == n)
+		return (i);
+	else if (min == max)
+		return (-1);
+	else if (i_sqr < n)
+		return (_sqrt_recursion_wrapper(n, i + 1, max));
+	else
+		return (_sqrt_recursion_wrapper(n, min, i - 1));
+}
+
+/**
+ * _sqrt_recursion - a function that returns the natural
+ *                   square root of a number
+ *
+ * @n: input number
+ *
+ * Return: neutral square root
+*/
 int _sqrt_recursion(int n)
 {
-	if (n < 0)
-		return (-1);
-	if (n == 0)
-		return (0);
-	if (n == 1)
+	if (n == 1)		
 		return (1);
-	return (power_operation(n, 2));
+	else if (n == 0)
+		return (0);
+	else if (n < 0)
+		return (-1);
+	else
+		return (_sqrt_recursion_wrapper(n, 1, n));
 }
