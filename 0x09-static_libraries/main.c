c7593c193eebcbc316cf3fc45b1f0b795e4bbf7d
kamalnorman334 <kamalnorman334@gmail.com>
2022-10-02 10:14:39 -0700

    static library libmy.a containing all the functions

--- 0x09-static_libraries/main.c
+++ 0x09-static_libraries/main.c
@@ -1,14 +1,7 @@
 #include "main.h"
-/*
- * main - entry
- *
- * description: prints at the end of the day
- *
- * return: 0
- */
 
 int main(void)
 {
-_puts("\"At the end of the day, my goal was to be the best hacker\"\n\t- Kevin Mitnick");
-return (0);
+    _puts("\"At the end of the day, my goal was to be the best hacker\"\n\t- Kevin Mitnick");
+    return (0);
 }
