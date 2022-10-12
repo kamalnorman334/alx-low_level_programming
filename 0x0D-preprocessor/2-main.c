5e1b720304398bda503afb9aeaca98252ba3c48b
kamalnorman334 <kamalnorman334@gmail.com>
2022-10-11 18:27:06 -0700

    file

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
 
-/* main - entry 
+/*
+ * main - entry 
  *
  * Description: Prints file from where it was compiled from
  *
  * return 0 
  */
 
-int main (void)
+int main(void)
 {
 	printf("%s\n", __FILE__);
-	return (0);
+	return(0);
 }
