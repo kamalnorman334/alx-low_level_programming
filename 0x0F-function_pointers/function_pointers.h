3dbf35f20d3a73c4f5cd8616a1b89753146c271a
kamalnorman334 <kamalnorman334@gmail.com>
2022-10-13 15:43:44 -0700

    print name

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -3,6 +3,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/*
+ * Desc: Header file containing prototypes for all functions
+ *       used in function_pointers directory.
+ */
+
 
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
