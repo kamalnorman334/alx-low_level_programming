b6b750c8fed3918021db9d7a939227a67103c15b
kamalnorman334 <kamalnorman334@gmail.com>
2022-12-13 09:28:06 -0800

    dynamic libraries

---
+++ 0x18-dynamic_libraries/3-islower.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _islower - checks for lower case characters
+ * @c: entry
+ *
+ * Description: prints lowercase letters
+ * Return: Always(0).
+ */
+
+int _islower(int c)
+{
+	return (c >= 'a' && c <= 'z');
+}
